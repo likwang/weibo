92a0fd77ebaba3a43413004341d74aeb1ff81d98
imclam <280676861@qq.com>
2019-04-02 02:19:26 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title', 'Home')
 @section('content')
-	<h1>home</h1>
+	<div class="jumbotron">
+		<h1>Hello Laravel</h1>
+		<p class="lead">
+			你已经进入Laravel世界
+		</p>
+		<p>
+			一切，将从这里开始。
+		</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+		</p>
+	</div>
 @stop
