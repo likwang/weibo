8f66af503e936c8fba4e6b025d85e2ad41f8e702
imclam <280676861@qq.com>
2019-04-02 08:04:47 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<img class="brand-icon" src="https://iocaffcdn.phphub.org//uploads/communities/WtC3cPLHzMbKRSZnagU9.png">
+	<a href="http://apricot-custard-42334.herokuapp.com/" target="_blank">刻意练习，每日精进</a>
+
+	<div class="float-right">
+		<a href="/about">关于</a>
+	</div>
+</footer>
