8496a87b3a156f5e25bff6cbef04ea05affb447b
imclam <280676861@qq.com>
2019-04-10 09:42:37 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -60,12 +60,11 @@ class UsersController extends Controller
 	{
 		$view = 'emails.confirm';
 		$data = compact('user');
-		$from = 'imclam@example.com';
 		$name = 'Imclam';
 		$to = $user->email;
 		$subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 		Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-		$message->from($from, $name)->to($to)->subject($subject);
+		$message->to($to)->subject($subject);
 		});
 	}
 
