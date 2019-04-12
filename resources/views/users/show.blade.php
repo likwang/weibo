38e28cdfcbc7328f7866ec23b039d64b7b3e1584
imclam <280676861@qq.com>
2019-04-12 09:27:18 +0000

    关注按钮

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.default')
 @section('title', $user->name)
+
 @section('content')
 <div class="row">
  	<div class="offset-md-2 col-md-8">
@@ -8,6 +9,9 @@
 	 			<section class="user_info">
 	 				@include('shared._user_info', ['user' => $user])
 	 			</section>
+	 			@if (Auth::check())
+	 				@include('users._follow_form')
+	 			@endif
 	 			<section class="stats mt-2">
  					@include('shared._stats', ['user' => $user])
  				</section>
